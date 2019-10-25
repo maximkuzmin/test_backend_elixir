73a59a176828efa424d2cb52d6d2dbc2320e8109
maximkuzmin <photographer.max@gmail.com>
2019-10-25 18:20:02 +0700

    Code linted with mix/format

--- lib/geospatial_api_web/controllers/jobs_in_radius_controller.ex
+++ lib/geospatial_api_web/controllers/jobs_in_radius_controller.ex
@@ -4,10 +4,12 @@ defmodule GeospatialApiWeb.JobsInRadiusController do
 
   def index(conn, params) do
     valid_params = validate_params(params)
+
     case valid_params do
       {:ok, params} ->
         data = Services.ClosestJobOffersToLocation.call(params)
         render(conn, "index.json", data: data)
+
       {:error, errors} ->
         conn |> put_status(400) |> render("error.json", errors: errors)
     end
@@ -21,8 +23,14 @@ defmodule GeospatialApiWeb.JobsInRadiusController do
       {data, types}
       |> Changeset.cast(params, Map.keys(types))
       |> Changeset.validate_required(Map.keys(types))
-      |> Changeset.validate_number(:lattitude, less_than_or_equal_to: 90.0, greater_than_or_equal_to: -90.0)
-      |> Changeset.validate_number(:longitude, less_than_or_equal_to: 90.0, greater_than_or_equal_to: -90.0)
+      |> Changeset.validate_number(:lattitude,
+        less_than_or_equal_to: 90.0,
+        greater_than_or_equal_to: -90.0
+      )
+      |> Changeset.validate_number(:longitude,
+        less_than_or_equal_to: 90.0,
+        greater_than_or_equal_to: -90.0
+      )
       |> Changeset.validate_number(:radius, greater_than_or_equal_to: 0)
 
     if changeset.valid? do
